e2d381c9c191402568f9cda5427ccdb9581e8660
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2023-09-11 06:59:58 +0000

    install vapor toolbox in devcontainer

--- .devcontainer/postCreateCommand.bash
+++ .devcontainer/postCreateCommand.bash
@@ -2,15 +2,30 @@
 
 set -euo pipefail
 
+APP_DIR=/workspaces/VaporDevcontainer
+SRC_DIR=/workspaces/src
+
+# Install Vapor Toolbox
+VAPOR_TOOLBOX_VERSION="18.7.1"
+cd $SRC_DIR
+if [ ! -d "toolbox" ]; then
+  git clone https://github.com/vapor/toolbox.git
+fi
+cd toolbox
+git checkout "tags/$VAPOR_TOOLBOX_VERSION"
+make install
+which vapor
+
 # Install apple/swift-format
-cd /workspaces/swift-format
-VERSION=508.0.0  # replace this with the version you need
+SWIFT_FORMAT_VERSION="508.0.0"
+cd $SRC_DIR
 if [ ! -d "swift-format" ]; then
   git clone https://github.com/apple/swift-format.git
 fi
 cd swift-format
-git checkout "tags/$VERSION"
+git checkout "tags/$SWIFT_FORMAT_VERSION"
 swift build -c release
 cp .build/release/swift-format /usr/local/bin/
 which swift-format
-cd /workspaces/VaporDevcontainer
+
+cd $APP_DIR
